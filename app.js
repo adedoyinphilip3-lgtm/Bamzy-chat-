4abe47006e96e60e9fd58510096cead83b90c3d1
adedoyinphilip3-lgtm <adedoyinphilip3@gmail.com>
2026-08-19 11:42:48 +0100

    Initialize Supabase client in app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const SUPABASE_URL = "YOUR_PROJECT_URL";
+const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 function sendMessage() {
   const input = document.getElementById("messageInput");
   const messages = document.getElementById("messages");
